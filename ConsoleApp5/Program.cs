fd7b986b623eac21d720ee5fa7828e2209f7ab7f
DmitriyHolub <6367394@mail.ru>
2021-02-20 10:33:37 +0300

    sklad training

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Sklad;
+
+namespace ConsoleApp5
+{
+ 
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Add_NEW_Product.AddNEWproductbase();
+            Display.StartProgram();
+
+
+        }
+        
+    }
+    
+}
